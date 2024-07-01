c68f6a95e12f34256279166a681fb802d85e8019
ymnk-kohey <k.ymnk1011@gmail.com>
2024-07-02 01:26:41 +0900

    制作の作業

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,21 @@
 /*
-splideの記述
+ページリロード時のスクロール保持の記述
 */
-document.addEventListener('DOMContentLoaded', function () {
-  new Splide('.splide', {
-      type: 'fade',
-      // perPage: 1,
-      autoplay: true, 
-      pauseOnHover: false,
-      rewind: true, //画像が最後まで行った場合、巻き戻って初めからスライドさせるかどうかの設定
-      rewindByDrag: true, //ドラッグまたはスワイプ操作でもスライダーを巻き戻せるかどうかの設定。「rewind」オプションを有効にしないと動作しない。
-      // width: '80%',
-      // height: '42rem',
-      speed: number = 1200 //画像と画像が切り替わるときのスピード設定
-  }).mount();
+// ページのDOMが読み込まれる前にスクロール位置を復元する
+document.addEventListener('DOMContentLoaded', function() {
+  const scrollPosition = localStorage.getItem('scrollPosition');
+  if (scrollPosition) {
+      window.scrollTo(0, parseInt(scrollPosition, 10));
+  }
+});
+// ページが完全に読み込まれた後にスクロールを有効にする
+window.addEventListener('load', function() {
+  document.documentElement.style.overflow = 'auto';
+  document.body.style.overflow = 'auto';
+});
+// ページがスクロールされたときにスクロール位置を保存する
+window.addEventListener('scroll', function() {
+  localStorage.setItem('scrollPosition', window.scrollY);
 });
 
 /*
@@ -22,7 +25,6 @@ document.addEventListener('DOMContentLoaded', function () {
 const header = document.getElementById("header");
 // ヘッダー要素の初期位置(ページの一番上からの距離)を取得
 const headerOffsetTop = header.offsetTop;
-
 // スクロール時にヘッダーを固定する関数
 function fixHeader() {
 // 現在のスクロール位置 (window.scrollY) がヘッダーの初期位置よりも大きいかチェック
@@ -34,9 +36,22 @@ function fixHeader() {
     header.style.top = "0";
   }
 }
-
 // スクロールイベントリスナーを追加する
 window.addEventListener("scroll", fixHeader);
+
+/*
+splideの記述
+*/
+document.addEventListener('DOMContentLoaded', function () {
+  new Splide('.splide', {
+      type: 'fade',
+      autoplay: true, 
+      pauseOnHover: false,
+      rewind: true, //画像が最後まで行った場合、巻き戻って初めからスライドさせるかどうかの設定
+      rewindByDrag: true, //ドラッグまたはスワイプ操作でもスライダーを巻き戻せるかどうかの設定。「rewind」オプションを有効にしないと動作しない。
+      speed: number = 1200 //画像と画像が切り替わるときのスピード設定
+  }).mount();
+});
 
 /*
 サイト内画像の右クリックを禁止
